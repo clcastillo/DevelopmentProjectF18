9c8e627d2897cb28410529f660d605dae5b65c04
AaronXFZ <43483348+AaronXFZ@users.noreply.github.com>
2018-10-11 22:40:05 -0700

    Add files via upload

---
+++ Yongyuan Zhang unfinished project3.py
@@ -0,0 +1,58 @@
+class Group_1(GroupMember):#Not Done yet. Will working on it tmr
+    list1 = ['Aaron', 'Luke', 'Shirley']
+    list2 = ['Computer Engineering', 'System Engineering']
+    def meet_up(day, time):
+        for member in list1:
+            if:
+
+                print("The time is avaliable for all of us!")
+            else:
+
+
+                print("That time doesn't work.")
+
+    def print_members(self):
+        for member in list1:
+            print("%a's major is %b. And his/her schedule is "(member.name, member.major))
+            memeber.print_schedule()
+                
+
+
+
+
+class GroupMember():
+    def __init__(self, name, major, class_Schedule[]):
+        self.name = name
+        self.major = major
+        self.class_Schedule[] = classSchedule[]
+        
+        
+    
+    def print_schedule(self):
+        for member in list1:
+            print("%s's class list is: ")
+            for classes in member.classSchedule:
+                print(classes+" ")
+                
+            
+
+
+
+
+
+
+class Class(groupMember):
+    def __init__(self,department, classNumber, startTime, endTime):
+        self.department = department
+        self.classNumber = classNumber
+        self.startTime = startTime[]
+        self.endTime = endTime[]
+
+    def print_class_info():
+        print("%s's class time is "(self.name))
+        for i in range(0, classNumber):
+            print("%d to % g" (startTime[i], endTime[i]))
+            
+
+    
+    
